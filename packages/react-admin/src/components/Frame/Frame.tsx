35597373b44c1922a6668d01f425fabc11776b9f
bobjoe12131 <bobjoegit@pm.me>
2025-07-02 16:00:20 -0500

    bleh

--- packages/react-admin/src/components/Frame/Frame.tsx
+++ packages/react-admin/src/components/Frame/Frame.tsx
@@ -183,7 +183,7 @@ export function PageRoot() {
           <Button
             onClick={handleClick}
             size="small"
-            sx={{ ml: 2 }}
+            sx={{ ml: 2, "text-transform": "none" }}
             aria-controls={open ? 'account-menu' : undefined}
             aria-haspopup="true"
             aria-expanded={open ? 'true' : undefined}
@@ -202,9 +202,9 @@ export function PageRoot() {
           anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
         >
           <MenuItem onClick={handleClickProfile}>
-            <ListItemIcon ><Avatar sx={{ width: 24, height: 24 }} /></ListItemIcon>
-            {indexJson.username}
-          </MenuItem>
+	  <ListItemIcon><PersonIcon fontSize="small" /></ListItemIcon>
+          Profile
+	  </MenuItem>
           {/* <MenuItem onClick={handleClose}>
             <ListItemIcon><Settings fontSize="small" /></ListItemIcon>Settings
           </MenuItem> */}
